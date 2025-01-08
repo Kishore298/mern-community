12a53f397f14636ca601771c2bd84914ef5cd6ed
Kishore298 <kishore103345@gmail.com>
2025-01-08 11:47:45 +0530

    first commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaBars, FaUserCircle, FaSignOutAlt } from "react-icons/fa";
-import { decode } from "jwt-decode";
+import decode from "jwt-decode";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
